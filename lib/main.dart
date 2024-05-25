6ae0ecd4c1ac206f7399d037209128a786c71c2b
jinwook song <wlsdnr129@naver.com>
2024-05-25 16:00:25 +0900

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -41,10 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -58,6 +54,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
