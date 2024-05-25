e3001563f0f45fcef9c5f0b2c812a2bf5bd9e84c
jinwook song <wlsdnr129@naver.com>
2024-05-25 20:31:59 +0900

    style(tabbar): ui

--- lib/presentation/screens/main/widgets/top_app_bar/home_app_bar.dart
+++ lib/presentation/screens/main/widgets/top_app_bar/home_app_bar.dart
@@ -1,9 +1,10 @@
-import 'package:e_commerce/core/theme/constant/app_colors.dart';
 import 'package:e_commerce/core/theme/constant/app_icons.dart';
+import 'package:e_commerce/core/theme/custom/custom_app_bar.dart';
+import 'package:e_commerce/core/theme/custom/custom_font_weight.dart';
 import 'package:e_commerce/presentation/screens/main/cubit/mall_type_cubit.dart';
+import 'package:e_commerce/presentation/screens/main/widgets/top_app_bar/svg_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 
 class HomeAppbar extends StatelessWidget {
   const HomeAppbar({super.key});
@@ -14,64 +15,75 @@ class HomeAppbar extends StatelessWidget {
       builder: (_, state) {
         return AnimatedContainer(
           duration: const Duration(milliseconds: 300),
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
           color: state.theme.backgroundColor,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-            child: AppBar(
-              backgroundColor: Colors.transparent,
-              leadingWidth: 86,
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(
-                  AppIcons.mainLogo,
-                  colorFilter: ColorFilter.mode(
-                    state.theme.logoColor,
-                    BlendMode.srcIn,
-                  ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            systemOverlayStyle: state.theme.systemUiOverlayStyle,
+            leadingWidth: 86,
+            leading: SvgIconButton(
+              icon: AppIcons.mainLogo,
+              color: state.theme.logoColor,
+              padding: 8,
+              onPressed: () {},
+            ),
+            centerTitle: true,
+            title: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              decoration: BoxDecoration(
+                color: state.theme.containerColor,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(CustomAppBarTheme.tabBarRadius),
                 ),
               ),
-              centerTitle: true,
-              title: DefaultTabController(
-                length: MallType.values.length,
-                initialIndex: state.index,
-                child: TabBar(
-                  onTap: (index) =>
-                      context.read<MallTypeCubit>().changeIndex(index),
-                  labelColor: AppColors.black,
-                  unselectedLabelColor: AppColors.black,
-                  tabs: List.generate(
-                    MallType.values.length,
-                    (index) => Tab(
-                      child: Text(
-                        MallType.values[index].label,
+              child: SizedBox(
+                height: 28,
+                child: DefaultTabController(
+                  length: MallType.values.length,
+                  initialIndex: state.index,
+                  child: TabBar(
+                    tabAlignment: TabAlignment.center,
+                    tabs: List.generate(
+                      MallType.values.length,
+                      (index) => Tab(
+                        text: MallType.values[index].label,
                       ),
                     ),
+                    indicator: BoxDecoration(
+                      color: state.theme.indicatorColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(CustomAppBarTheme.tabBarRadius),
+                      ),
+                    ),
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    dividerColor: Colors.transparent,
+                    labelColor: state.theme.labelColor,
+                    labelStyle: Theme.of(context).textTheme.labelLarge.bold,
+                    labelPadding: const EdgeInsets.symmetric(horizontal: 12),
+                    unselectedLabelColor: state.theme.unselectedLabelColor,
+                    unselectedLabelStyle:
+                        Theme.of(context).textTheme.labelLarge,
+                    onTap: (index) =>
+                        context.read<MallTypeCubit>().changeIndex(index),
+                    splashBorderRadius: const BorderRadius.all(
+                      Radius.circular(CustomAppBarTheme.tabBarRadius),
+                    ),
                   ),
                 ),
               ),
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SvgPicture.asset(
-                    AppIcons.location,
-                    colorFilter: ColorFilter.mode(
-                      state.theme.logoColor,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SvgPicture.asset(
-                    AppIcons.cart,
-                    colorFilter: ColorFilter.mode(
-                      state.theme.logoColor,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                ),
-              ],
             ),
+            actions: [
+              SvgIconButton(
+                icon: AppIcons.location,
+                color: state.theme.iconColor,
+                onPressed: () {},
+              ),
+              SvgIconButton(
+                icon: AppIcons.cart,
+                color: state.theme.iconColor,
+                onPressed: () {},
+              ),
+            ],
           ),
         );
       },
