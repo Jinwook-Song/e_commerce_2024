1f741a1acaea5682cf5542d340f4adc5be9304d7
jinwook song <wlsdnr129@naver.com>
2024-05-26 02:08:12 +0900

    feat(get_it): service locator 패턴

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:e_commerce/core/theme/theme_data.dart';
 import 'package:e_commerce/core/utils/logging.dart';
-import 'package:e_commerce/data/data_source/mock/display/display.mock_api.dart';
-import 'package:e_commerce/data/repository_impl/display_repository_impl.dart';
+import 'package:e_commerce/domain/usecase/display/display.usecase.dart';
+import 'package:e_commerce/domain/usecase/display/menu/get_menus.usecase.dart';
 import 'package:e_commerce/presentation/routes/router.dart';
 import 'package:e_commerce/presentation/screens/main/cubit/mall_type_cubit.dart';
+import 'package:e_commerce/service_locator.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  final data = await DisplayRepositoryImpl(DisplayMockApi())
-      .getMenusByMallType(mallType: MallType.market);
-  logging(data);
+  setLocator();
+
+  final menus = await locator<DisplayUsecase>().execute(
+    usecase: GetMenusUsecase(MallType.market),
+  );
+
+  logging(menus);
+
   runApp(const MainApp());
 }
 
