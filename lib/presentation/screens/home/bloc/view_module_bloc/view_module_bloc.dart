16927d1616c1f044d0a526e4ea9afb63eacc1475
jinwook song <wlsdnr129@naver.com>
2024-06-01 22:33:10 +0900

    feat(presentation): view_module bloc

---
+++ lib/presentation/screens/home/bloc/view_module_bloc/view_module_bloc.dart
@@ -0,0 +1,58 @@
+import 'package:e_commerce/core/utils/constants.dart';
+import 'package:e_commerce/core/utils/error/error_response.dart';
+import 'package:e_commerce/core/utils/exception/common_exception.dart';
+import 'package:e_commerce/core/utils/logging.dart';
+import 'package:e_commerce/domain/model/common/result.dart';
+import 'package:e_commerce/domain/model/display/display.model.dart';
+import 'package:e_commerce/domain/usecase/display/display.usecase.dart';
+import 'package:e_commerce/domain/usecase/display/view_module/get_view_modules.usecase.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'view_module_event.dart';
+part 'view_module_state.dart';
+part 'view_module_bloc.freezed.dart';
+
+class ViewModuleBloc extends Bloc<ViewModuleEvent, ViewModuleState> {
+  final DisplayUsecase _displayUsecase;
+
+  ViewModuleBloc(this._displayUsecase) : super(ViewModuleState());
+
+  Future<Result<List<ViewModule>>> _fetch(int tabId) async {
+    return await _displayUsecase.execute(usecase: GetViewModulesUsecase(tabId));
+  }
+
+  Future<void> _onViewModuleInitialized(
+    ViewModuleInitialized event,
+    Emitter<ViewModuleState> emit,
+  ) async {
+    emit(state.copyWith(status: Status.loading));
+    final int tabId = event.tabId;
+
+    try {
+      final response = await _fetch(tabId);
+      response.when(
+        success: (viewModules) {
+          emit(
+            state.copyWith(
+              status: Status.success,
+              tabId: tabId,
+              viewModules: viewModules,
+            ),
+          );
+        },
+        failure: (error) {
+          emit(state.copyWith(status: Status.error, error: error));
+        },
+      );
+    } catch (e) {
+      logging(e, logType: LogType.error);
+      emit(
+        state.copyWith(
+          status: Status.error,
+          error: CommonException.setError(e),
+        ),
+      );
+    }
+  }
+}
---
+++ lib/presentation/screens/home/bloc/view_module_bloc/view_module_event.dart
@@ -0,0 +1,10 @@
+part of 'view_module_bloc.dart';
+
+abstract class ViewModuleEvent {
+  const ViewModuleEvent();
+}
+
+class ViewModuleInitialized extends ViewModuleEvent {
+  final int tabId;
+  ViewModuleInitialized(this.tabId);
+}
