ee6d8aa11151f4588615dc391c0ad063d6601e46
jinwook song <wlsdnr129@naver.com>
2024-06-16 19:29:34 +0900

    swagger: api docs

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_swagger_ui/shelf_swagger_ui.dart';
 
 import 'dto/product_info.dto.dart';
 import 'dto/view_module.dto.dart';
@@ -20,10 +21,18 @@ final _router = Router()
 void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
+  // Swagger handler
+  final swagger = SwaggerUI('specs/swagger.yaml', title: 'Swagger');
+
+  // Combine Swagger and other routes
   final handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  // Combine the two handlers
+  final cascade = Cascade().add(handler).add(swagger.call).handler;
 
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
+  final server = await serve(cascade, ip, port);
+
   print('✅ Server listening on http://localhost:${server.port}');
 }
