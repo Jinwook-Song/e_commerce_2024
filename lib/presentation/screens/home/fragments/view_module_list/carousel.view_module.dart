6980a5eee8f85da2afe8c8bceadf4aed60d69cb5
jinwook song <wlsdnr129@naver.com>
2024-06-23 18:30:56 +0900

    feat(gesture): RawGestureDetector, CustomGestureRecognizer

--- lib/presentation/screens/home/fragments/view_module_list/carousel.view_module.dart
+++ lib/presentation/screens/home/fragments/view_module_list/carousel.view_module.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 
 import 'package:e_commerce/core/theme/constant/app_colors.dart';
-import 'package:e_commerce/core/utils/logging.dart';
 import 'package:e_commerce/core/utils/widgets/common_image.dart';
 import 'package:e_commerce/domain/model/display/display.model.dart';
 import 'package:e_commerce/domain/model/display/product_info/product_info.model.dart';
 import 'package:e_commerce/presentation/screens/home/fragments/view_module_list/factory/view_module_widget.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class CarouselViewModlue extends StatefulWidget with ViewModuleWidget {
@@ -25,16 +25,13 @@ class _CarouselViewModlueState extends State<CarouselViewModlue> {
   void initState() {
     super.initState();
     _timer = _periodicTimer();
-    _pageController.addListener(_handleUserScroll);
   }
 
   @override
   void dispose() {
-    _pageController
-      ..removeListener(_handleUserScroll)
-      ..dispose();
-    super.dispose();
     _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
   }
 
   Timer _periodicTimer() {
@@ -60,35 +57,71 @@ class _CarouselViewModlueState extends State<CarouselViewModlue> {
   @override
   Widget build(BuildContext context) {
     final List<ProductInfo> products = widget.info.products;
-    return AspectRatio(
-      aspectRatio: 375 / 345,
-      child: Stack(
-        children: [
-          PageView.builder(
-            controller: _pageController,
-            onPageChanged: (page) {
-              setState(() {
-                _currentPage = page % products.length + 1;
-              });
-            },
-            itemBuilder: (context, index) {
-              // final src = products[index % products.length].imageUrl;
-              final src =
-                  'https://i.pravatar.cc/150?img=${index % 5 + 2 - 1}%E2%80%8B';
-              return CommonImage(src);
-            },
-          ),
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: PageCountWidget(
-              currentPage: _currentPage,
-              totalPage: products.length,
+    return RawGestureDetector(
+      gestures: {
+        CustomGestureRecognizer:
+            GestureRecognizerFactoryWithHandlers<CustomGestureRecognizer>(
+          () => CustomGestureRecognizer(),
+          (instance) {
+            instance.onDown = (details) {
+              if (_timer.isActive) {
+                _timer.cancel();
+              }
+            };
+
+            // 유저가 drag하지 않고 손을 떼는 경우
+            instance.onCancel = () {
+              if (!_timer.isActive) {
+                _timer = _periodicTimer();
+              }
+            };
+
+            instance.onEnd = (details) {
+              if (!_timer.isActive) {
+                _timer = _periodicTimer();
+              }
+            };
+          },
+        ),
+      },
+      child: AspectRatio(
+        aspectRatio: 375 / 345,
+        child: Stack(
+          children: [
+            PageView.builder(
+              controller: _pageController,
+              onPageChanged: (page) {
+                setState(() {
+                  _currentPage = page % products.length + 1;
+                });
+              },
+              itemBuilder: (context, index) {
+                // final src = products[index % products.length].imageUrl;
+                final src =
+                    'https://i.pravatar.cc/150?img=${index % 5 + 2 - 1}%E2%80%8B';
+                return CommonImage(src);
+              },
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: PageCountWidget(
+                currentPage: _currentPage,
+                totalPage: products.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class CustomGestureRecognizer extends HorizontalDragGestureRecognizer {
+  @override
+  void rejectGesture(int pointer) {
+    // recognizer가 패배하였더러도, 승리자 리스트에 추가
+    acceptGesture(pointer);
   }
 }
 
